f0c67247e7728e19ebc291a86f148f78c71f94ec
joshuashevchuk <joshua.shevchuk@ookla.com>
2025-05-31 17:02:34 -0700

    simple clause to check convo history

--- src/clients/openai.py
+++ src/clients/openai.py
@@ -5,16 +5,29 @@ class OpenaiClient:
     def __init__(self):
         self.client = OpenAI()
 
-    async def _post_space(self):
-        url = "http://localhost:8000/document/12345/convo_history"
+    def _get_transcript(self,meet_key):
+        url = f"http://localhost:8000/document/{meet_key}/transcript"
         headers = {
             "accept": "application/json",
             "Content-Type": "application/json"
         }
         data = {
-            "meet_key": self.meet_key,
-            "space_name": self.space_name,
-            "space_uri": self.space_uri,
+            "meet_key": meet_key,
+        }
+
+        response = requests.post(url, headers=headers, json=data)
+        if response.status_code != "200":
+            return None
+        return response
+
+    def _get_convo_history(self,meet_key):
+        url = f"http://localhost:8000/document/{meet_key}/convo_history"
+        headers = {
+            "accept": "application/json",
+            "Content-Type": "application/json"
+        }
+        data = {
+            "meet_key": meet_key,
         }
 
         response = requests.post(url, headers=headers, json=data)
@@ -23,25 +36,32 @@ class OpenaiClient:
 
     async def text_chat(self, text: str, meet_key: str):
         messages = []
-        messages.append({
-                    "role": "developer",
-                    "content": [
-                        {
-                            "type": "text",
-                            "text": """
-                                You are a helpful and supportive teammate the speaks with enthusiasm and excitement!
-                                Your name is Gene
-                                You are a 10x Developer and Software Engineer, so you might not know everything, be humble about that.
-                                You will be provided with a transcript from a google meet, and your task is to use the meeting transcript to provide an insightful answer to the users question.
-                                You will simplify and provide an easily understandable answer.
-                            """
-                        }
-                    ]
-                })
-        messages.append({
-                    "role": "user",
-                    "content": "Meeting Transcript: " + "hello world"
-                })
+        convo_resp = self._get_convo_history(meet_key)
+        transcript_res = self._get_transcript(meet_key)
+
+        if convo_resp is not None:
+            messages.append(convo_resp.text)
+        elif transcript_res is not None and convo_resp is None:
+            messages.append({
+                "role": "developer",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": """
+                                           You are a helpful and supportive teammate the speaks with enthusiasm and excitement!
+                                           Your name is Gene
+                                           You are a 10x Developer and Software Engineer, so you might not know everything, be humble about that.
+                                           You will be provided with a transcript from a google meet, and your task is to use the meeting transcript to provide an insightful answer to the users question.
+                                           You will simplify and provide an easily understandable answer.
+                                       """
+                    }
+                ]
+            })
+            messages.append({
+                "role": "user",
+                "content": "Meeting Transcript: " + transcript_res.content
+            })
+
         messages.append({
             "role": "user",
             "content": "User Message: " + text
